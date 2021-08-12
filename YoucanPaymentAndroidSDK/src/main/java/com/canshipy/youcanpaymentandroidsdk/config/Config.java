429bb12c5894c82e5ff524bb516c463ac4f92acd
Abdelmjid-ASOUAB <abde.asouabgmail.com>
2021-08-12 13:48:51 +0100

    add account id to Config

--- YoucanPaymentAndroidSDK/src/main/java/com/canshipy/youcanpaymentandroidsdk/config/Config.java
+++ YoucanPaymentAndroidSDK/src/main/java/com/canshipy/youcanpaymentandroidsdk/config/Config.java
@@ -9,6 +9,7 @@ public class Config {
     public static String CURRENCY = "MAD";
     public static String ORDER_ID = "111";
     public static String ACCOUNT_ID = "24499aee-e76d-42d0-8d55-f62e37b6a6d3";
+    public static String PUB_KEY = "5d90d854-a449-4ed4-a48b-6488e3ecfa48";
 
 
     static {
