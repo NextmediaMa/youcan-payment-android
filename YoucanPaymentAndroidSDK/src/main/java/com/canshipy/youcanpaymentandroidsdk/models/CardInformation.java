64354cee34adc0df76591813dfe24145d4e79d4c
Abdelmjid-ASOUAB <abde.asouabgmail.com>
2021-08-16 10:01:33 +0100

    add getter and setter to CardInformation

--- YoucanPaymentAndroidSDK/src/main/java/com/canshipy/youcanpaymentandroidsdk/models/CardInformation.java
+++ YoucanPaymentAndroidSDK/src/main/java/com/canshipy/youcanpaymentandroidsdk/models/CardInformation.java
@@ -2,18 +2,47 @@ package com.canshipy.youcanpaymentandroidsdk.models;
 
 public class CardInformation {
 
-    String cardHolderName="";
-    String cardNumber="";
-    String expireDate="";
-    String cvv="";
-
-    public CardInformation() {
-    }
+    private String cardHolderName="";
+    private String cardNumber="";
+    private String expireDate="";
+    private String cvv="";
 
     public CardInformation(String cardHolderName, String cardNumber, String expireDate, String cvv) {
         this.cardHolderName = cardHolderName;
         this.cardNumber = cardNumber;
         this.expireDate = expireDate;
+        this.cvv = cvv;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(String expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
         this.cvv = cvv;
     }
 }
