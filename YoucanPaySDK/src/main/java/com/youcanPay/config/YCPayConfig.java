8e34e5af34ee3b4292a443d6d76e9ed225116e57
abde-asouab <abde.asouab@gmail.com>
2022-02-15 17:52:24 +0100

    migration to youcanpay.com

--- YoucanPaySDK/src/main/java/com/youcanPay/config/YCPayConfig.java
+++ YoucanPaySDK/src/main/java/com/youcanPay/config/YCPayConfig.java
@@ -1,7 +1,7 @@
 package com.youcanPay.config;
 
 public class YCPayConfig {
-    public static String API_URL = "https://pay.youcan.shop/";
+    public static String API_URL = "https://youcanpay.com/";
     public static String API_URL_SANDBOX = API_URL + "sandbox/";
     public static String YCP_TAG = "YCPay";
 }
